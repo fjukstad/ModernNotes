a2bcb68c06511aedefa4875e2df6610457ff4742
Bjørn Fjukstad <bjorn@cs.uit.no>
2018-01-31 22:34:34 +0100

    swagger docs

--- Controllers/NoteController.cs
+++ Controllers/NoteController.cs
@@ -15,18 +15,22 @@ namespace ModernNotes.Controllers
             _context = context; 
         }
 
-        /// <summary>
-        /// Get all notes.!
-        /// </summary> 
+        /// <summary>Retrieve all notes.</summary>
+        /// <returns>A list of all notes.</returns>
+        [ProducesResponseType(typeof(IEnumerable<Note>), 200)]
         [HttpGet("/api/notes")]
         public IEnumerable<Note> GetAll()
         {
             return _context.Notes.ToList(); 
         }
 
+        /// <summary>Retrieve a specific note.</summary>
+        /// <returns>The note with the given id.</returns>
+        /// <response code="200">If the note is found.</response>            
+        /// <response code="400">If the note is not found.</response>     
+        [ProducesResponseType(typeof(Note), 200)]
+        [ProducesResponseType(typeof(void), 400)]       
         [HttpGet("/api/notes/{id}", Name = "Get")]
-        /// <summary>
-        /// shit shit shit </summary> 
         public Note Get(long id)
         {
             var item = _context.Notes.FirstOrDefault(t => t.Id == id);
@@ -37,7 +41,13 @@ namespace ModernNotes.Controllers
             return item;
        }
 
-       [HttpPost("/api/new")]
+        /// <summary>Create a new note.</summary>
+        /// <returns>The newly created note.</returns>
+        /// <response code="201">If the note is created successfully.</response>            
+        /// <response code="400">If the note is null.</response>     
+        [ProducesResponseType(typeof(Note), 201)]
+        [ProducesResponseType(typeof(void), 400)]       
+        [HttpPost("/api/new")]
         public IActionResult New([FromBody] Note note){
             if(note==null){
                 return BadRequest();
@@ -47,6 +57,12 @@ namespace ModernNotes.Controllers
             return CreatedAtRoute("View", new { id = note.Id}, note);
         }
 
+        /// <summary>Delete a note.</summary>
+        /// <returns></returns>
+        /// <response code="204">If the note is deleted successfully.</response>            
+        /// <response code="400">If the note is not found.</response>     
+        [ProducesResponseType(typeof(void), 204)]
+        [ProducesResponseType(typeof(void), 400)]  
         [HttpPost("/api/delete/{id}")]
         public IActionResult Delete(int id) { 
             var note = Get(id);
@@ -58,6 +74,14 @@ namespace ModernNotes.Controllers
             return new NoContentResult();
         }
 
+        /// <summary>Update a note.</summary>
+        /// <returns></returns>
+        /// <response code="204">If the note is updated successfully.</response>            
+        /// <response code="400">If the note is null or the id match the id of the note.</response>     
+        /// <response code="404">If the note is not found.</response>     
+        [ProducesResponseType(typeof(void), 204)]
+        [ProducesResponseType(typeof(void), 400)]  
+        [ProducesResponseType(typeof(void), 404)]  
         [HttpPut("/api/update/{id}")]
         public IActionResult Update (int id, [FromBody] Note updateNote) { 
             if(updateNote == null || updateNote.Id != id) { 
@@ -77,7 +101,8 @@ namespace ModernNotes.Controllers
             return new NoContentResult();
 
         }
-        
+
+       [ApiExplorerSettings(IgnoreApi = true)]
        [HttpGet]
        [Route("/notes")]  
        public IActionResult Notes(){
@@ -85,6 +110,7 @@ namespace ModernNotes.Controllers
             return View();
         }
 
+       [ApiExplorerSettings(IgnoreApi = true)]
        [HttpGet]
        [Route("/note/{id}", Name="View")]  
        public IActionResult Note(int id){
@@ -97,6 +123,7 @@ namespace ModernNotes.Controllers
         }
 
 
+       [ApiExplorerSettings(IgnoreApi = true)]
        [HttpGet]
        [Route("/edit/{id}", Name="Edit")]  
        public IActionResult Edit(int id){
